2d30b8815d9dc8976fe72231549de0713ce526ff
edeldev <edeldev25@gmail.com>
2025-09-20 13:39:16 -0600

    fix: bugId

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,26 +1,24 @@
-import { v4 as uuidv4 } from "uuid";
-
 export const hamburguesas = [
   {
-    id: uuidv4(),
+    id: "h1",
     text: "Hamburguesa sencilla",
     price: 55,
     img: "/hamburguesa.webp",
   },
   {
-    id: uuidv4(),
+    id: "h2",
     text: "Hamburguesa sencilla con papas",
     price: 70,
     img: "/hamburguesa.webp",
   },
   {
-    id: uuidv4(),
+    id: "h3",
     text: "Hamburguesa doble",
     price: 70,
     img: "/hamburguesa.webp",
   },
   {
-    id: uuidv4(),
+    id: "h4",
     text: "Hamburguesa doble con papas",
     price: 85,
     img: "/hamburguesa.webp",
@@ -29,14 +27,14 @@ export const hamburguesas = [
 
 export const alitas = [
   {
-    id: uuidv4(),
+    id: "a1",
     text: "Orden de boneless",
     price: 110,
     img: "/boneless.webp",
     sauces: ["BBQ", "Buffalo", "Mango"],
   },
   {
-    id: uuidv4(),
+    id: "a2",
     text: "Orden de alitas",
     price: 135,
     img: "/alitas.webp",
@@ -46,79 +44,79 @@ export const alitas = [
 
 export const tacos = [
   {
-    id: uuidv4(),
+    id: "t1",
     text: "Orden tacos de maíz de bistec",
     price: 85,
     img: "/tacos-erick.webp",
   },
   {
-    id: uuidv4(),
+    id: "t2",
     text: "Orden tacos de harina de bistec",
     price: 80,
     img: "/orden-harina.webp",
   },
   {
-    id: uuidv4(),
+    id: "t3",
     text: "Orden tacos de maíz de trompo",
     price: 85,
     img: "/tacos-trompo-maiz.webp",
   },
   {
-    id: uuidv4(),
+    id: "t4",
     text: "Orden tacos de harina de trompo",
     price: 85,
     img: "/tacos-trompo.webp",
   },
   {
-    id: uuidv4(),
+    id: "t5",
     text: "Enchiladas",
     price: 80,
     img: "/enchiladas.webp",
   },
   {
-    id: uuidv4(),
+    id: "t6",
     text: "Pirata chica",
     price: 20,
     img: "/gringa.webp",
   },
   {
-    id: uuidv4(),
+    id: "t7",
     text: "Pirata grande",
     price: 20,
     img: "/gringa.webp",
   },
   {
-    id: uuidv4(),
+    id: "t8",
     text: "Gringa chica",
     price: 35,
     img: "/gringa.webp",
   },
   {
-    id: uuidv4(),
+    id: "t9",
     text: "Gringa grande",
     price: 80,
     img: "/gringa.webp",
   },
   {
-    id: uuidv4(),
+    id: "t10",
     text: "Campechana chica",
     price: 80,
     img: "/gringa.webp",
   },
   {
-    id: uuidv4(),
+    id: "t11",
     text: "Campechana grande",
     price: 80,
     img: "/gringa.webp",
   },
   {
-    id: uuidv4(),
+    id: "t12",
     text: "Papas fritas",
     price: 55,
     img: "/papas.webp",
   },
   {
-    id: uuidv4(),
+    id: "t13",
     text: "Papas fritas con queso y carne",
     price: 85,
     img: "/papas-queso.webp",
